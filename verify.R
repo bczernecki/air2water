12cfdf4997da45ec5395bb5cada420248fc6c1e8
Your Name <bczernecki@gmail.com>
2017-12-01 08:46:43 +0100

    verify

--- verify.R
+++ verify.R
@@ -1,2 +1,31 @@
-# R script to verify the air2water simulation for the Charzykowskie lake:
+# R script to verify the air2water simulation for the Hancza lake:
 
+# walidacja danych archiwalnych dla recenzentow:
+column_names <-  c("yy","mm","dd","temp_p_obs", "temp_j_obs","temp_j_air2water",  "temp_p_aggr", "temp_j_aggr")
+air2water_cal <- na.omit(read.table("Hancza/output_2/2_PSO_RMS_hancza_sat_cc_1d.out", na.strings = "-999.00000", col.names = column_names))
+air2water_val  <- na.omit(read.table("Hancza/output_2/3_PSO_RMS_hancza_sat_cv_1d.out", na.strings = "-999.00000", col.names = column_names))
+
+head(air2water_cal)
+
+library(dplyr)
+summary_mth_cal <- air2water_cal %>% group_by(yy,mm) %>% summarise_all(., mean) 
+summary_mth_val <- air2water_val %>% group_by(yy,mm) %>% summarise_all(., mean) 
+
+
+library(verification)
+plot(verify(obs = summary_mth_cal$temp_j_obs, pred = summary_mth_cal$temp_j_air2water, frcst.type = "cont",obs.type = "cont"))
+plot(verify(obs = summary_mth_val$temp_j_obs, pred = summary_mth_val$temp_j_air2water, frcst.type = "cont",obs.type = "cont"))
+
+
+
+#a <- verify(obs = podsumowanie$temp_j_obs, pred = podsumowanie$temp_j_air2water, frcst.type = "cont",obs.type = "cont")
+
+# wynik_model_rec <- readRDS(file="data/wynik_model_rec_hist.rds")
+# charzykowskie <- wynik_model_rec %>% filter(nazwa=="Charzykowskie") 
+# head(charzykowskie)
+# head(podsumowanie)
+# charzykowskie <- left_join(charzykowskie, podsumowanie)
+# charzykowskie <- charzykowskie %>% filter(yy>=2000)
+# 
+# 
+# 
