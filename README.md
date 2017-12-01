cd4fb749e032ee75f88c72cd02fa3445f5a02b8a
bczernecki <bczernecki@gmail.com>
2017-12-01 09:36:33 +0100

    verify

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # air2water
-Problem
+
+The problem with running air2water model beyond calibration period... 
+Model shows almost no skill in simulating lake surface water temperature (LSWT) after year 2000
+
+Simulation files are provided in the folders entitle with the name of each of lakes (Lubie, Studzieniczne, Charzykowskie, Mikolajskie, Gardno, Selmet, Hancza, Selmet.Wielki, Sepolenskie, Jeziorak, Slawskie)
+
+Parameters established with the provided Matlab code according to the mean depth of lake (as given in the pre_processing folder)
+
+Below some diagnostic plots obtained with the code included in the file verify.R:
+
+![Lake Hancza - Calibration - (1972-2000)](hancza_calibration.png)
+![Lake Hancza - Validation - (2001-2015)](hancza_validation.png)
+
--- verify.R
+++ verify.R
@@ -1,9 +1,10 @@
 # R script to verify the air2water simulation for the Hancza lake:
 
-# walidacja danych archiwalnych dla recenzentow:
+# reading the calibration and validation results:
 column_names <-  c("yy","mm","dd","temp_p_obs", "temp_j_obs","temp_j_air2water",  "temp_p_aggr", "temp_j_aggr")
 air2water_cal <- na.omit(read.table("Hancza/output_2/2_PSO_RMS_hancza_sat_cc_1d.out", na.strings = "-999.00000", col.names = column_names))
 air2water_val  <- na.omit(read.table("Hancza/output_2/3_PSO_RMS_hancza_sat_cv_1d.out", na.strings = "-999.00000", col.names = column_names))
+
 
 head(air2water_cal)
 
@@ -13,19 +14,24 @@ summary_mth_val <- air2water_val %>% group_by(yy,mm) %>% summarise_all(., mean)
 
 
 library(verification)
-plot(verify(obs = summary_mth_cal$temp_j_obs, pred = summary_mth_cal$temp_j_air2water, frcst.type = "cont",obs.type = "cont"))
-plot(verify(obs = summary_mth_val$temp_j_obs, pred = summary_mth_val$temp_j_air2water, frcst.type = "cont",obs.type = "cont"))
+png("hancza_calibration.png", height = 800, width = 800, pointsize = 20)
+plot(verify(obs = summary_mth_cal$temp_j_obs, pred = summary_mth_cal$temp_j_air2water, frcst.type = "cont",
+            obs.type = "cont"),  main="Conditional Quantile Plot - calibration period\nHancza Lake")
+dev.off()
 
+png("hancza_validation.png", height = 800, width = 800, pointsize = 20)
+plot(verify(obs = summary_mth_val$temp_j_obs, pred = summary_mth_val$temp_j_air2water, frcst.type = "cont",
+             obs.type = "cont"), main="Conditional Quantile Plot - validation period\nHancza Lake")
+dev.off()
 
-
+ 
+######################################################
+# everything below is not important for this case... #
+######################################################
 #a <- verify(obs = podsumowanie$temp_j_obs, pred = podsumowanie$temp_j_air2water, frcst.type = "cont",obs.type = "cont")
-
 # wynik_model_rec <- readRDS(file="data/wynik_model_rec_hist.rds")
 # charzykowskie <- wynik_model_rec %>% filter(nazwa=="Charzykowskie") 
 # head(charzykowskie)
 # head(podsumowanie)
 # charzykowskie <- left_join(charzykowskie, podsumowanie)
 # charzykowskie <- charzykowskie %>% filter(yy>=2000)
-# 
-# 
-# 
